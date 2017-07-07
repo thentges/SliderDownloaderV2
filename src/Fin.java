8c19b2da7ee4f022265c1101ef02bd79be526f56
Thibault HENTGES <thibault.hentges@gmail.com>
2017-07-07 08:40:15 +0200

    Revert "revert"
    This reverts commit 710efadaecb9166ccc77548a60a118893227659b.

--- src/Fin.java
+++ src/Fin.java
@@ -34,7 +34,7 @@ public void paintComponent(Graphics g){
 	    g.drawString(strFooter, (300 - metricsFooter.stringWidth(strFooter))/2, 370);   
 	    // LOGO HEADER
 	    try {
-	        Image img = ImageIO.read(new File("/Ressources/slider_logo.png"));
+	        Image img = ImageIO.read(new File("slider_logo.png"));
 	        g.drawImage(img, 43, 0, this);
 	      } catch (IOException e) {
 	        e.printStackTrace();
