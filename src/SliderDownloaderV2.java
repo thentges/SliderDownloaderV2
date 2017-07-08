8048b1539e743827194685f8f1080943b0766373
Thibault HENTGES <thibault.hentges@gmail.com>
2017-07-08 13:32:58 +0200

    createObjetProgramme fini

--- src/SliderDownloaderV2.java
+++ src/SliderDownloaderV2.java
@@ -1,6 +1,5 @@
 import java.awt.Color;
 import java.awt.Desktop;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -14,7 +13,6 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -22,7 +20,6 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 
@@ -177,22 +174,18 @@ public class SliderDownloaderV2 {
 	
 	
 	
-	// ARRAYS pour execution du programme
-	
-	static ArrayList<String> liste_liens = new ArrayList<String>(); // liste des liens sliders
-	static ArrayList<String> liste_erreur = new ArrayList<String>(); // liste des erreurs
-	
-	static int choix=0;
+	static int choix=0; 
+	// choix = 1 > lance le programme
+	// choix = 2 > revient a la page d'accueil
 	
 	public static void main(String[] args) {
 		
-		//INITIALISATION
+		File filePath = new File("Ressources/path.txt"); // fichier dans lequel le chemin de la liste de sons est enregistré
+		File fileErreur = new File("liste_erreur.txt"); // liste des erreurs
 		
-		File filePath = new File("Ressources/path.txt");
-		File fileErreur = new File("liste_erreur.txt");
+// initialisation des vues
 		
-
-		//FENETRE
+		//	FENETRE
 		JFrame fenetre = new JFrame(); 
 	    fenetre.setTitle("SliderDownloader"); 
 	    fenetre.setBackground(Color.BLACK);
@@ -200,12 +193,16 @@ public class SliderDownloaderV2 {
 	    fenetre.setLocationRelativeTo(null); // PLACE LA FENETRE AU CENTRE
 	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stoppe le programme a la fermeture (croix rouge) 
 	    fenetre.setResizable(false);
-	    Accueil accueil = new Accueil();
 	    
-	    // boite de dialogue
+	    //	PANELS
+	    Accueil accueil = new Accueil(); // création écran accueil
+	    Chargement chargement = new Chargement(); // création écran de chargement
+	    Fin end = new Fin(); // création écran de fin
+	    
+	    //	BOITE DE DIALOGUES
 	    JOptionPane mustChoose = new JOptionPane();
 	    
-	    // SELECTEUR DE .TXT
+	    //	SELECTEUR DE .TXT
 	    JFileChooser selecteur = new JFileChooser();
 		FileNameExtensionFilter filtre = new FileNameExtensionFilter("Fichiers .txt", "txt");
 		selecteur.setMultiSelectionEnabled(false);
@@ -214,7 +211,7 @@ public class SliderDownloaderV2 {
         selecteur.setFileFilter(filtre);
         selecteur.setDialogTitle("Choississez une liste de sons au format .txt");
 	    
-	    //BOUTON OUVRIR LE FICHIER
+	    //	BOUTON OUVRIR LE FICHIER
 	    JButton btnOpen = new JButton("Ouvrir le fichier");
 	    btnOpen.setBounds(50, 170, 200, 20);
 	    btnOpen.addActionListener(new ActionListener() {
@@ -222,13 +219,12 @@ public class SliderDownloaderV2 {
 			public void actionPerformed(ActionEvent e) { //LORSQU'ON CLIQUE SUR LE BTN
 				if(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(java.awt.Desktop.Action.OPEN)){
 					open(new File(getFirstLine(filePath)));
-				} 
-				
+				} 	
 			}
-	         });
+	    });
 	    
 	    
-	    //BOUTON SELECTIONNER UN FICHIER
+	    //	BOUTON SELECTIONNER UN FICHIER
 	    JButton btnSelect = new JButton("Selectionner un autre fichier");
 	    btnSelect.setBounds(50, 200, 200, 20);
 	    btnSelect.addActionListener(new ActionListener()  {
@@ -236,10 +232,10 @@ public class SliderDownloaderV2 {
 			public void actionPerformed(ActionEvent e) { //LORSQU'ON CLIQUE SUR LE BTN
 				savePath(selecteur, filePath);
 			}
-	         });
+	    });
 	    
 	    
-	    // BOUTON LANCER LE PROGRAMME
+	    //	BOUTON LANCER LE PROGRAMME
 	    JButton btnLaunch = new JButton("Analyser les morceaux");
 	    btnLaunch.setBounds(50, 230, 200, 70); 
 	    btnLaunch.addActionListener(new ActionListener() {
@@ -249,9 +245,9 @@ public class SliderDownloaderV2 {
 				choix=1;
 				System.out.println(choix);
 			}
-	         });
+	    });
 	    
-	 // BOUTON OUVRIR LISTE ERREUR
+	    //	BOUTON OUVRIR LISTE ERREUR
 		JButton btnErreur = new JButton("Ouvrir la liste des erreurs");
 		btnErreur.setBounds(50, 260, 200, 25);
 	    btnErreur.addActionListener(new ActionListener() {
@@ -261,10 +257,9 @@ public class SliderDownloaderV2 {
 					open(fileErreur);
 				}
 			}
-	         });
+	    });
 	    
-	    
-	    // BOUTON retour a l'accueil
+	    // 	BOUTON RETOUR À L'ACCUEIL
 	    JButton btnRetour = new JButton("Retour à l'accueil");
 	    btnRetour.setBounds(50, 300, 200, 25);
 	    btnRetour.addActionListener(new ActionListener() {
@@ -272,24 +267,29 @@ public class SliderDownloaderV2 {
 			public void actionPerformed(ActionEvent e) { //LORSQU'ON CLIQUE SUR LE BTN
 				choix = 2;
 			}
-	         });
+	    });
 	    
-	 // BOUTON TELECHARGER 
+	    //	 BOUTON TELECHARGER 
 		JButton btnDL = new JButton("Telecharger les morceaux");
-			btnDL.setBounds(50, 190, 200, 50);
-		    btnDL.addActionListener(new ActionListener() {
+		btnDL.setBounds(50, 190, 200, 50);
+		    
+// fin de l'initialisation des vues    
+		
+	    do{
+	    	Programme prog = new Programme(filePath);
+	    	btnDL.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) { //LORSQU'ON CLIQUE SUR LE BTN
 					if(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(java.awt.Desktop.Action.BROWSE)){
-						for (int i=0 ; i<liste_liens.size() ; i++)  {
-						open(liste_liens.get(i));
+						for (int i=0 ; i<prog.getliste_liens().size() ; i++)  {
+						open(prog.getliste_liens().get(i));
+						}
+						if (!prog.getliste_liens().isEmpty()){
+							prog.getliste_liens().clear();
 						}
 					}
 				}
-		         });
-		    
-		    
-	    do{
+	    	});
 	    	if (getFirstLine(filePath)!=null){ // si un chemin est enregistré
 				System.out.println("path.txt contient un chemin");
 				File fileSons = new File(getFirstLine(filePath)); //crée un fichier à partir de ce chemin
@@ -318,80 +318,33 @@ public class SliderDownloaderV2 {
 	    while (choix!=1){ // tant que le le bouton lancer le programme n'a pas été pressé
 	    	System.out.println("choix pas égal a 1"); 
 	    } // sort quand le bouton a été pressé
-	    Chargement chargement = new Chargement(); // création écran de chargement
+	    
+	    
 	    fenetre.setContentPane(chargement); // afficher l'écran de chargement
 	    fenetre.validate();
 	    
-	    // PROGRAMME COMMENCE ICI
-    	System.out.println("LE PROGRAMME COMMENCE"); // pour test
-		try{ 
-
-			BufferedReader buff = new BufferedReader(new FileReader(getFirstLine(filePath))); 
-
-			try { 
-				String line;
-				int compteurLignes=0;
-				int compteurErreur=0;
-				ArrayList<Morceau> liste_morceaux = new ArrayList<Morceau>();
-
-				while ( (line = buff.readLine()) != null){	// tant que la ligne lue n'est pas nulle
-				  liste_morceaux.add(new Morceau(line)); // crée un nouvel objet Morceau
-				  chargement.setCompteurLignes(compteurLignes); 
-				  chargement.setNom(line);
-				  chargement.repaint();
-				  liste_morceaux.get(compteurLignes).setSource(); // recupere code source de la page après execution du JS
-				  liste_morceaux.get(compteurLignes).parse(); // parse le code source pour en extraire le lien de dl
-				  if (liste_morceaux.get(compteurLignes).getSuccess()) { // si le programme a trouvé un lien
-				    liste_liens.add(liste_morceaux.get(compteurLignes).getLink()); // ajoute le lien a la liste de liens
-				    compteurLignes = compteurLignes + 1; //MAJ le compteur
-				  }
-				  else { // si le programme n'a pas trouvé de lien
-				    compteurErreur = compteurErreur + 1; //MAJ les compteurs
-				    compteurLignes = compteurLignes + 1;
-				    liste_erreur.add(line); // ajoute le nom du morceau à la liste des erreurs
-				    
-				  }
-				}
-					ecrire(fileErreur, liste_erreur, "\n"); // ecrit la liste des erreurs dans le fichier liste_erreur			
-					playSound("Ressources/ah_denis.wav"); // joue un son
-					ecrire(new File(getFirstLine(filePath)) , ""); // vide la liste des de l'utilisateur
-					Fin end = new Fin(); 
-					end.setCompteurLignes(compteurLignes);
-					end.setCompteurErreur(compteurErreur);
-					fenetre.setContentPane(end); //affiche l'écran de fin
-					fenetre.getContentPane().setLayout(null);
+	    prog.execute(chargement); // lancement du programme
+	    
+	    // le programme est terminé
+		ecrire(fileErreur, prog.getliste_erreur(), "\n"); // ecrit la liste des erreurs dans le fichier liste_erreur			
+		playSound("Ressources/ah_denis.wav"); // joue le fameux "AH" de Denis Brogniart
+		ecrire(new File(getFirstLine(filePath)) , ""); // vide la liste des de l'utilisateur
 					
+		end.setCompteurLignes(prog.getCompteurLignes());
+		end.setCompteurErreur(prog.getCompteurErreur());
 					
-					    fenetre.getContentPane().add(btnDL);
-					
-					
-					 fenetre.getContentPane().add(btnErreur);
-				    fenetre.getContentPane().add(btnRetour);
-				    
-				fenetre.validate();
-					
-					buff.close(); //Lecture fini donc on ferme le flux 
-				} 
-
-				catch (IOException e){ 
-					System.out.println(e.getMessage()); 
-					System.exit(1); 
-				} 
-		
-		} 
-				catch (IOException e) { 
-					System.out.println(e.getMessage()); 
-					System.exit(1); 
-			} 
-		while (choix!=2){
-	    	System.out.println("choix pas égal a 2"); 
-	    }
+		fenetre.setContentPane(end); //affiche l'écran de fin
+		fenetre.getContentPane().setLayout(null);			
+		// ajoute les différents boutons à l'écran de fin
+		fenetre.getContentPane().add(btnDL);
+		fenetre.getContentPane().add(btnErreur);
+		fenetre.getContentPane().add(btnRetour);
+		fenetre.validate();
+							
+		while (choix!=2){ // tant que l'utilisateur n'a pas cliqué sur "retour à l'accueil"
+	    	System.out.println("choix pas égal a 2"); // reste dans la boucle
+	    } // sort de la boucle quand l'utilisateur a cliqué, et relance donc le "do...while"
 		}while(choix==2);
-
-	    	
-	    
-	    
-		
+	
 	}
-
 }
